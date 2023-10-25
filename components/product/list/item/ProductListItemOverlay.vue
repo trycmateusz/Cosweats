ecae571124078d61193f5cd8d90c7ddc8153f92c
Mateusz Tryc <mateusz.tryc420@gmail.com>
2023-10-25 14:23:25 +0200

    removed on pointervent from zooming in on images

--- components/product/list/item/ProductListItemOverlay.vue
+++ components/product/list/item/ProductListItemOverlay.vue
@@ -97,7 +97,7 @@ const activePhotoSrc = computed(() => {
 const activePhotoAlt = computed(() => {
   return `${props.product.name} in ${productStore.active?.color}`
 })
-const zoomInActivePhoto = (e: MouseEvent | TouchEvent) => {
+const zoomInActivePhoto = (e: MouseEvent) => {
   const scale = 2
   if (activePhoto.value) {
     activePhoto.value.style.transform = `scale(${scale}) translate(${scale * 0.25 * -e.offsetX}px, ${scale * 0.25 * -e.offsetY}px)`
